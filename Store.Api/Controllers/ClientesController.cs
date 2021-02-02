8e1c05d86e0800d64d448c8d6dcced49ae5ea44a
Cledson Pacheco <cledson.caetano@gmail.com>
2021-02-02 17:34:22 -0300

    exemplo de versionamento de api

---
+++ Store.Api/Controllers/ClientesController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Store.Api.Model;
+using System;
+
+namespace Store.Api.Controllers
+{
+    [ApiController]
+    [Route("api/clientes")]
+    [ApiVersion("1.0", Deprecated = true)]
+    [ApiVersion("2.0")]
+    public class ClientesController : ControllerBase
+    {
+        [HttpGet("{clienteId}")]
+        public IActionResult ObterClienteV1([FromRoute] Guid clienteId)
+        {
+            var cliente = new ClienteModel
+            {
+                Id = clienteId,
+                Nome = "Nome do Cliente"
+            };
+
+            return Ok(cliente);
+        }
+
+        [HttpGet("{clienteId}")]
+        [MapToApiVersion("2.0")]
+        public IActionResult ObterClienteV2([FromRoute] Guid clienteId)
+        {
+            var cliente = new ClienteModelV2
+            {
+                Id = clienteId,
+                Nome = "Nome do Cliente v2",
+                Idade = 19
+            };
+
+            return Ok(cliente);
+        }
+    }
+}
+
---
+++ Store.Api/Model/ClienteModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Store.Api.Model
+{
+    public class ClienteModel
+    {
+        public Guid Id { get; set; }
+        public string Nome { get; set; }
+    }
+}
